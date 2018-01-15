b5f9c1da984d2b0275671f324fd69632c6988f9c
Ryan Hodges <ryan.d.hodges@gmail.com>
2018-01-15 15:29:29 -0800

    fixing textfield admin widgets across all forms

--- Shikari/people/admin.py
+++ Shikari/people/admin.py
@@ -1,59 +1,54 @@
 from django.contrib import admin
 from .models import *
+from django.forms.widgets import TextInput
 
-class OrganizationWebsiteInline(admin.TabularInline):
+class TextField2TextInput(admin.ModelAdmin):
+    # I like to use TextFields to avoid restricting character counts, but I like TextInput widget
+    formfield_overrides = {
+        models.TextField: {'widget': TextInput},
+    }
+
+class TextField2TextInputInline(admin.TabularInline):
+    # I like to use TextFields to avoid restricting character counts, but I like TextInput widget
+    formfield_overrides = {
+        models.TextField: {'widget': TextInput},
+    }
+
+class OrganizationWebsiteInline(TextField2TextInputInline):
     model = OrganizationWebsite
     fields = ('website', 'site_type')
     extra = 0
-    # classes = ['collapse', 'open']
     verbose_name_plural = 'Websites'
-    def formfield_for_dbfield(self, db_field, **kwargs):
-        from django.forms.widgets import TextInput
-        if db_field.name in ['site_type']:
-            kwargs['widget'] = TextInput
-        return super(OrganizationWebsiteInline,self).formfield_for_dbfield(db_field, **kwargs)
 
-class OrganizationAdmin(admin.ModelAdmin):
+class OrganizationAdmin(TextField2TextInput):
     list_display = ('name', 'is_home')
     list_filter = ['is_home', 'name']
     search_fields = ('name',)
     verbose_name_plural = 'Oranizations'
-    def formfield_for_dbfield(self, db_field, **kwargs):
-        from django.forms.widgets import TextInput
-        if db_field.name in ['name']:
-            kwargs['widget'] = TextInput
-        return super(OrganizationAdmin,self).formfield_for_dbfield(db_field, **kwargs)
     inlines = [
         OrganizationWebsiteInline,
     ]
 
 admin.site.register(Organization, OrganizationAdmin)
-admin.site.register(WebsiteType)
-admin.site.register(OrganizationWebsite)
+admin.site.register(WebsiteType, TextField2TextInput)
+admin.site.register(OrganizationWebsite, TextField2TextInput)
 
-class PhoneNumberInline(admin.TabularInline):
+class PhoneNumberInline(TextField2TextInputInline):
     model = PhoneNumber
     fields = ('number', 'contact_type', 'is_primary',)
     extra = 1
     verbose_name_plural = 'Phone Numbers'
-    def formfield_for_dbfield(self, db_field, **kwargs):
-        from django.forms.widgets import TextInput
-        if db_field.name in ['number']:
-            kwargs['widget'] = TextInput
-        return super(PhoneNumberInline,self).formfield_for_dbfield(db_field, **kwargs)
+    formfield_overrides = {
+        models.TextField: {'widget': TextInput},
+    }
 
-class EmailAddressInline(admin.TabularInline):
+class EmailAddressInline(TextField2TextInputInline):
     model = EmailAddress
     fields = ('email', 'contact_type', 'is_primary',)
     extra = 1
     verbose_name_plural = 'Email Addresses'
-    def formfield_for_dbfield(self, db_field, **kwargs):
-        from django.forms.widgets import TextInput
-        if db_field.name in ['email']:
-            kwargs['widget'] = TextInput
-        return super(EmailAddressInline,self).formfield_for_dbfield(db_field, **kwargs)
 
-class PersonAdmin(admin.ModelAdmin):
+class PersonAdmin(TextField2TextInput):
     list_display = ('last_name', 'first_name', 'name_suffix', 'job_title', 'organization')
     list_filter = ('last_name', 'first_name', 'name_suffix', 'job_title', 'organization')
     search = ('last_name', 'first_name', 'middle_name', 'name_suffix', 'job_title', 'organization', 'manager', 'notes')
@@ -73,18 +68,13 @@ class PersonAdmin(admin.ModelAdmin):
             'fields': ('notes',)
         }),
     )
-    def formfield_for_dbfield(self, db_field, **kwargs):
-        from django.forms.widgets import TextInput
-        if db_field.name in ['name_prefix', 'first_name', 'middle_name', 'last_name', 'name_suffix', 'job_title', 'job_class']:
-            kwargs['widget'] = TextInput
-        return super(PersonAdmin,self).formfield_for_dbfield(db_field, **kwargs)
     inlines = [
         PhoneNumberInline,
         EmailAddressInline,
     ]
 
 admin.site.register(Person, PersonAdmin)
-admin.site.register(PhoneContactType)
-admin.site.register(EmailContactType)
-admin.site.register(PhoneNumber)
-admin.site.register(EmailAddress)
+admin.site.register(PhoneContactType, TextField2TextInput)
+admin.site.register(EmailContactType, TextField2TextInput)
+admin.site.register(PhoneNumber, TextField2TextInput)
+admin.site.register(EmailAddress, TextField2TextInput)
